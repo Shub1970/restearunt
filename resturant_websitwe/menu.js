c02e7700fb9c8c7b2ff3ea45eeaddff3c5024b9c
Shub1970 <18shubham1998@gmail.com>
2023-03-16 07:37:19 +0530

    complet code

---
+++ resturant_websitwe/menu.js
@@ -0,0 +1,58 @@
+const menu = [
+  {
+    category: 1,
+    id: "indian_dish",
+    food: [
+      "aloo paratha",
+      "chicken tikka masala",
+      "saag paneer",
+      "biryani",
+      "dosa",
+    ],
+    rank: [1, 2, 3, 4, 5],
+  },
+  {
+    category: 2,
+    id: "chinese_dish",
+    food: [
+      "kung pao chicken",
+      "mapo tofu",
+      "hot and sour soup",
+      "dim sum",
+      "fried rice",
+    ],
+    rank: [1, 2, 3, 4, 5],
+  },
+  {
+    category: 3,
+    id: "european_dish",
+    food: [
+      "spaghetti carbonara",
+      "fish and chips",
+      "paella",
+      "shepherd's pie",
+      "schnitzel",
+    ],
+    rank: [1, 2, 3, 4, 5],
+  },
+  {
+    category: 4,
+    id: "american_dish",
+    food: [
+      "hamburger",
+      "hot dog",
+      "mac and cheese",
+      "buffalo wings",
+      "grilled cheese ",
+    ],
+    rank: [1, 2, 3, 4, 5],
+  },
+  {
+    category: 5,
+    id: "south_american_dish",
+    food: ["tacos", "empanadas", "feijoada", "ceviche", "picanha"],
+    rank: [0, 1, 2, 3, 4],
+  },
+];
+
+export default menu;
